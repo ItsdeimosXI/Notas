c4a4be470cadfadbac3a2a164ea659e540e27250
Deimos <salamancamanuel310@gmail.com>
2024-05-14 21:03:30 -0300

    Store login - incompleto

--- frontend/src/components/Login/LoginVue.vue
+++ frontend/src/components/Login/LoginVue.vue
@@ -2,13 +2,13 @@
     <form>
         <!-- Email input -->
         <div data-mdb-input-init class="form-outline mb-4">
-          <input type="email" id="form2Example1" class="form-control" />
-          <label class="form-label" for="form2Example1">Email address</label>
+          <input type="text" id="form2Example1" class="form-control" v-model="user"/>
+          <label class="form-label" for="form2Example1" >Usuario</label>
         </div>
       
         <!-- Password input -->
         <div data-mdb-input-init class="form-outline mb-4">
-          <input type="password" id="form2Example2" class="form-control" />
+          <input type="password" id="form2Example2" class="form-control" v-model="password" {{store.password}}/>
           <label class="form-label" for="form2Example2">Password</label>
         </div>
       
@@ -29,11 +29,15 @@
         </div>
       
         <!-- Submit button -->
-        <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+        <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4" {{ store.Login()}} >Sign in</button>
 
     </form>
 </template>
 
 <script setup lang="ts">
-
+import { UseAuthStore } from '@/stores/store';
+import { ref } from 'vue';
+const user = ref('')
+const password = ref('')
+const store = UseAuthStore
 </script>
--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -1,17 +1,49 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-
+import axios from 'axios'
 export const UseAuthStore = defineStore('Auth', {
-  state: () => 
-    (
-      {Logged: false }
-    ),
-    actions: {
-      async Login(username, password){
-        try{
-          const res = await fetch()
-        }
-      }
+  state: () =>
+  (
+    {
+      authUser: null
     }
-   
+
+  ),
+  getters: {
+    user: (state) => state.authUser
+  },
+  actions: {
+    async GetToken(){
+      await axios.get("api/token")
+    },
+    async GetUser()
+    async Login(username: string, password:string): Promise<boolean> {
+      try {
+        const res = await fetch('http://127.0.0.1:8000/api/api-auth/login/', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password
+          })
+        })
+        const response = await res.json()
+        if ('non_field_errors' in response) {
+            this.error = "Login failed"
+            return false
+            }else{
+              this.Token = response.data.key
+              return true
+            }
+      }
+      catch(error){
+        this.error = "Login failed"
+        return false
+      }
+      }
+  }
+
 })
